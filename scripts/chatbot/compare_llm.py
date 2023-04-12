fd2dc4ff58d04b593a055cc0d36574e8f4ab0f6a
Vision AI Labs 전재현 <jerome.jun@sk.com>
2023-04-12 18:24:18 +0900

    Live mode

--- scripts/chatbot/compare_llm.py
+++ scripts/chatbot/compare_llm.py
@@ -14,6 +14,10 @@ class ConversationBot:
         scripts_len = self.agent.base_memory_script_len()
         self.user_history[:scripts_len] = self.agent.get_memory_script(ups_summary, conv_summary)
 
+    def get_base_scripts(self):
+        scripts_len = self.agent.base_memory_script_len()
+        return self.user_history[:scripts_len]
+
     def clear_history(self):
         del self.user_history[self.agent.base_memory_script_len():]
 
@@ -24,44 +28,28 @@ class ConversationBot:
 
 
 if __name__ == "__main__":
-#     input = "어제 내가 어딜 다녀왔는지 알아?"
-#     prompt = \
-# f"""아래 정보 중 질문과 관련있는 내용들만 활용하여 AI의 답변을 완성하시오.
-
-# 나는 어제 저녁에 버스를 타고 백화점에 다녀옴.
-# 백화점에서 간식과 빵, 고기를 구매함.
-# 오래전에 축구를 하다 정강이가 부러져 고생함.
-# 아내는 새우 알러지가 있음.
-
-# 나: {input}
-# AI:
-# """
-#     print("[INPUT]")
-#     print(prompt)
-#     print()
-#     print("[USER] : {}".format(input))
-
-#     glm_api.request_inference(prompt)
-#     openai_api.request_inference(prompt)
-
     adot_agent = glm_api.AdotAgent("http://172.27.30.115", "철수")
     adot_bot = ConversationBot(adot_agent)
     chatgpt_agent = openai_api.ChatGPTAgent("철수")
     chatgpt_bot = ConversationBot(chatgpt_agent)
 
-    ups_summary = "나는 30대 남성이며 어쿠스틱 음악을 좋아함. 특히 피아노 치는것이 취미이며 쇼팽을 존경함."
-    conv_summary = "나는 축구를 하다 정강이가 부러졌었음. 나는 자장면, 파스타를 싫어함. 아내는 새우 알러지가 있음."
+    ups_summary = "나는 9살 남자 어린이이며 6살 동생이 있음."
+    conv_summary = "나는 포켓몬스터를 좋아함. 포켓몬스터 스티커를 모으는데 동생이 자꾸 뺏음."
+
     adot_bot.update_s0(ups_summary, conv_summary)
     chatgpt_bot.update_s0(ups_summary, conv_summary)
+    base_scripts = adot_bot.get_base_scripts()
 
-    messages = [
-        "아내와 저녁식사를 하려는데, 새우 요리가 어떨까?",
-        "그럼 뭐가 좋을까?",
-    ]
+    for script in base_scripts:
+        print(f"[HUMAN]   : {script['user']}")
+        print(f"[BOT]     : {script['agent']}")
 
-    for m in messages:
-        print(f"[HUMAN]   : {m}")
-        adot_out = adot_bot.conversation(m)
-        print(f"[ADOT]    : {adot_out}")
-        chatgpt_out = chatgpt_bot.conversation(m)
-        print(f"[CHATGPT] : {chatgpt_out}")
+    while True:
+        try:
+            m = input("[HUMAN]   : ")
+            adot_out = adot_bot.conversation(m)
+            print(f"[ADOT]    : {adot_out}")
+            chatgpt_out = chatgpt_bot.conversation(m)
+            print(f"[CHATGPT] : {chatgpt_out}")
+        except KeyboardInterrupt:
+            break
