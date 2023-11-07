c7796c2c3f81251667b242a8c2eb666944d181c4
Jaehyun Jun <btjhjeon@gmail.com>
2023-11-07 15:08:54 +0900

    Convert MSC3.5 2nd data (23.10.03)

--- scripts/language/personalization/convert_data.py
+++ scripts/language/personalization/convert_data.py
@@ -46,130 +46,137 @@ def convert(
     output_train_path = os.path.join(data_dir, f"{data_name}_train{ext}")
     output_test_path = os.path.join(data_dir, f"{data_name}_test{ext}")
 
-    data = []
+    datas = []
     for path in data_path:
         with open(path, "r", encoding='utf-8-sig') as f:
             reader = csv.DictReader(f)
-            data.extend([d for d in reader])
+            datas.append([d for d in reader])
     
-    args.test_end_idx = args.test_end_idx if args.test_end_idx >= 0 else len(data)
+    args.test_end_idx = args.test_end_idx if args.test_end_idx >= 0 else len(datas)
     target_indices = list(range(args.test_start_idx, args.test_end_idx))
     random.shuffle(target_indices)
     test_indices = target_indices[-args.test_size:]
 
     content_id = None
-    session_no = 0
+    session_id = None
     count = 0
     jsonl_data_train = []
     jsonl_data_test = []
     json_data = None
-    for i, d in enumerate(data):
-        content_id_prev = content_id
-        content_id = d[HEADER_ID].strip() 
-        assert content_id, f"Empty row in line no {i+2}"
-        content = d[HEADER_CONTENT].strip() if HEADER_CONTENT in d else d[HEADER_CONTENT_EDIT].strip()
+    for data_no, data in enumerate(datas):
+        for i, row in enumerate(data):
+            content_id_prev = content_id
+            content_id = row[HEADER_ID].strip() 
+            assert content_id, f"Empty row in line no {i+2} of \"{data_path[data_no]}\""
+            content = row[HEADER_CONTENT].strip() if HEADER_CONTENT in row else row[HEADER_CONTENT_EDIT].strip()
 
-        if d[HEADER_SESS]:
-            if json_data is not None:
-                if count in test_indices:
-                    jsonl_data_test.append(json_data)
-                else:
-                    jsonl_data_train.append(json_data)
-                count += 1
-
-            session_no_prev = session_no
-            session_no = int(d[HEADER_SESS])
-            if session_no <= session_no_prev:
-                session_no += 300
-
-            user_name = d[HEADER_USER_NAME].strip().replace('[', '').replace(']이에요', '')
-            user = OrderedDict()
-            user["id"] = USER_ID
-            user["name"] = user_name
-            user["is_bot"] = False
-            bot = OrderedDict()
-            bot["id"] = BOT_ID
-            bot["name"] = BOT_NAME
-            bot["is_bot"] = True
-
-            json_data = OrderedDict()
-            json_data["source"] = "selectstar"
-            json_data["file_name"] = output_path
-            json_data["doc_id"] = str(session_no)
-            json_data["meta"] = OrderedDict()
-            json_data["meta"]["negative_data"] = False
-            json_data["meta"]["bot_tone"] = d[HEADER_TONE_BOT].strip()
-            json_data["meta"]["user_tone"] = d[HEADER_TONE_USER].strip()
-            json_data["meta"]["datetime"] = d[HEADER_DATETIME].strip()
-            json_data["meta"]["personas"] = [d[HEADER_USER_NAME].strip(), content]
-            json_data["speakers"] = OrderedDict()
-            json_data["speakers"][USER_ID] = user
-            json_data["speakers"][BOT_ID] = bot
-            json_data["talks"] = []
-            json_data["target"] = 1
-
-            assert content_id == "P1", f"the ID of start row should be \"P1\", but {content_id}"
-            ups_sum = content
-            conv_sum = ""
-
-        elif content_id.startswith("P"):
-            persona_no = int(content_id[1:])
-            if persona_no <= 3:
-                ups_sum += f" {content}"
-            else:
-                conv_sum += f" {content}"
-            json_data["meta"]["personas"].append(content)
-
-            if persona_no == 5:
-                if count in test_indices:
-                    template_no = 2
-                else:
-                    template_no = random.randint(1, len(CONTROL_TEMPLATE_CLASSES))
-                template = build_prompt_template(template_no)
-                json_data["meta"]["template_no"] = template_no
-                initial_script = template.generate(user_name, BOT_NAME, ups_sum, conv_sum)
-
-                for p, u in initial_script:
-                    if p == USER_PREFIX:
-                        json_data["talks"].append({
-                            "speaker": USER_ID,
-                            "msg": u,
-                            "ctrl": [],
-                            "not_trainable": 32
-                        })
-                    elif p == AGENT_PREFIX:
-                        json_data["talks"].append({
-                            "speaker": BOT_ID,
-                            "msg": u,
-                            "ctrl": [],
-                            "not_trainable": 32
-                        })
+            if row[HEADER_SESS]:
+                if json_data is not None:
+                    if count in test_indices:
+                        jsonl_data_test.append(json_data)
                     else:
-                        raise NotImplementedError(f"Unknown user type: {p}")
-                assert json_data["talks"][-1]["speaker"] == BOT_ID
-        
-        elif content_id.startswith("s"):
-            if "u" in content_id:
-                json_data["talks"].append({
-                    "speaker": USER_ID,
-                    "msg": content,
-                    "ctrl": [],
-                    "not_trainable": 32
-                })
-            elif "b" in content_id:
-                if content_id != content_id_prev:
+                        jsonl_data_train.append(json_data)
+                    count += 1
+
+                session_id_prev = session_id
+                session_id = f"{data_no+1}_{row[HEADER_SESS]}"
+
+                user_name = row[HEADER_USER_NAME].strip().replace('[', '').replace(']이에요', '')
+                user = OrderedDict()
+                user["id"] = USER_ID
+                user["name"] = user_name
+                user["is_bot"] = False
+                bot = OrderedDict()
+                bot["id"] = BOT_ID
+                bot["name"] = BOT_NAME
+                bot["is_bot"] = True
+
+                json_data = OrderedDict()
+                json_data["source"] = "selectstar"
+                json_data["file_name"] = output_path
+                json_data["doc_id"] = session_id
+                json_data["meta"] = OrderedDict()
+                json_data["meta"]["negative_data"] = False
+                json_data["meta"]["bot_tone"] = row[HEADER_TONE_BOT].strip()
+                json_data["meta"]["user_tone"] = row[HEADER_TONE_USER].strip()
+                json_data["meta"]["datetime"] = row[HEADER_DATETIME].strip()
+                json_data["meta"]["personas"] = [row[HEADER_USER_NAME].strip(), content]
+                json_data["speakers"] = OrderedDict()
+                json_data["speakers"][USER_ID] = user
+                json_data["speakers"][BOT_ID] = bot
+                json_data["talks"] = []
+                json_data["target"] = 1
+
+                assert content_id == "P1", f"the ID of start row should be \"P1\", but {content_id}"
+                ups_sum = content
+                conv_sum = ""
+
+            elif content_id.startswith("P"):
+                persona_no = int(content_id[1:])
+                if persona_no <= 3:
+                    ups_sum += f" {content}"
+                else:
+                    conv_sum += f" {content}"
+                json_data["meta"]["personas"].append(content)
+
+                if persona_no == 5:
+                    if count in test_indices:
+                        template_no = 2
+                    else:
+                        template_no = random.randint(1, len(CONTROL_TEMPLATE_CLASSES))
+                    template = build_prompt_template(template_no)
+                    json_data["meta"]["template_no"] = template_no
+                    initial_script = template.generate(user_name, BOT_NAME, ups_sum, conv_sum)
+
+                    for p, u in initial_script:
+                        if p == USER_PREFIX:
+                            json_data["talks"].append({
+                                "speaker": USER_ID,
+                                "msg": u,
+                                "ctrl": [],
+                                "not_trainable": 32
+                            })
+                        elif p == AGENT_PREFIX:
+                            json_data["talks"].append({
+                                "speaker": BOT_ID,
+                                "msg": u,
+                                "ctrl": [],
+                                "not_trainable": 32
+                            })
+                        else:
+                            raise NotImplementedError(f"Unknown user type: {p}")
+                    assert json_data["talks"][-1]["speaker"] == BOT_ID
+            
+            elif content_id.startswith("s"):
+                if "u" in content_id:
                     json_data["talks"].append({
-                        "speaker": BOT_ID,
+                        "speaker": USER_ID,
                         "msg": content,
                         "ctrl": [],
-                        "not_trainable": 0
+                        "not_trainable": 32,
+                        "meta": {"ID": content_id}
                     })
-                    if d[HEADER_RP_ID].strip():
-                        json_data["talks"][-1]['meta'] = {"RP_ID": d[HEADER_RP_ID].strip()}
+                elif "b" in content_id:
+                    if content_id != content_id_prev:
+                        json_data["talks"].append({
+                            "speaker": BOT_ID,
+                            "msg": content,
+                            "ctrl": [],
+                            "not_trainable": 0,
+                            "meta": {"ID": content_id}
+                        })
+                        if row[HEADER_RP_ID].strip():
+                            json_data["talks"][-1]['meta']["RP_ID"] = row[HEADER_RP_ID].strip()
+                    else:
+                        pre_content = json_data["talks"][-1]["msg"]
+                        pre_last_sent = pre_content.split("\n")[-1].strip()
+                        cur_first_sent = content.split("\n")[0].strip()
+                        if pre_last_sent.startswith("-") or pre_last_sent[0].isdigit() or \
+                           cur_first_sent.startswith("-") or cur_first_sent[0].isdigit():
+                            json_data["talks"][-1]["msg"] += "\n"
+                        json_data["talks"][-1]["msg"] += f"\n{content}"
                 else:
-                    json_data["talks"][-1]["msg"] += f"\n{content}"
-            else:
-                raise NotImplementedError(f"Invalid ID: {content_id}")
+                    raise NotImplementedError(f"Invalid ID: {content_id}")
     if count in test_indices:
         jsonl_data_test.append(json_data)
     else:
